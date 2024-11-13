95354abc3453d302a74974f97319e1c7ebf3d8d9
Madhusudhanan Padmanabhan <madhu_p@outlook.com>
2024-11-13 12:41:36 +0000

    5.4 commit

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.ChatCompletion;
-
+using Microsoft.SemanticKernel.Embeddings;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -42,24 +42,33 @@ builder.Services.AddSingleton<CosmosClient>((_) =>
  builder.Services.AddSingleton<Kernel>((_) =>
  {
      IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
-     kernelBuilder.AddAzureOpenAIChatCompletion(
-         deploymentName: builder.Configuration["AzureOpenAI:DeploymentName"]!,
-         endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
-         apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
-     );
+     
+     #pragma warning disable SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+     kernelBuilder.AddAzureOpenAITextEmbeddingGeneration(
+        deploymentName: builder.Configuration["AzureOpenAI:EmbeddingDeploymentName"]!,
+        endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
+        apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
+        );
+    #pragma warning restore SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+    //  kernelBuilder.AddAzureOpenAIChatCompletion(
+    //      deploymentName: builder.Configuration["AzureOpenAI:DeploymentName"]!,
+    //      endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
+    //      apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
+    //  );
      kernelBuilder.Plugins.AddFromType<DatabaseService>();
      return kernelBuilder.Build();
  });
 
 // Create a single instance of the AzureOpenAIClient to be shared across the application.
-builder.Services.AddSingleton<AzureOpenAIClient>((_) =>
-{
-    var endpoint = new Uri(builder.Configuration["AzureOpenAI:Endpoint"]!);
-    var credentials = new AzureKeyCredential(builder.Configuration["AzureOpenAI:ApiKey"]!);
+// builder.Services.AddSingleton<AzureOpenAIClient>((_) =>
+// {
+//     var endpoint = new Uri(builder.Configuration["AzureOpenAI:Endpoint"]!);
+//     var credentials = new AzureKeyCredential(builder.Configuration["AzureOpenAI:ApiKey"]!);
 
-    var client = new AzureOpenAIClient(endpoint, credentials);
-    return client;
-});
+//     var client = new AzureOpenAIClient(endpoint, credentials);
+//     return client;
+// });
 
 var app = builder.Build();
 
